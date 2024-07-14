64a8713edf94a5f0e767d757c33c2cc2d53d5323
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-07-14 21:04:08 +0200

    Issue1 - initial commit of gateway project, just from spring initializer.

---
+++ src/main/java/org/diecastfinder/scggateway/Scggateway2Application.java
@@ -0,0 +1,13 @@
+package org.diecastfinder.scggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scggateway2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scggateway2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/diecastfinder/scggateway/Scggateway2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diecastfinder.scggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scggateway2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
