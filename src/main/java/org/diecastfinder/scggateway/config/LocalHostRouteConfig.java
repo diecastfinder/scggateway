f5775d32d8793ebc77bc06f7e5d68c218c5c72fd
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-07-16 20:18:16 +0200

    issue3: add circuit-breaker behaviour

--- src/main/java/org/diecastfinder/scggateway/config/LocalHostRouteConfig.java
+++ src/main/java/org/diecastfinder/scggateway/config/LocalHostRouteConfig.java
@@ -14,7 +14,12 @@ public class LocalHostRouteConfig {
             .route(crawler -> crawler.path("/api/v1/crawler/*")
                 .uri("http://localhost:8083"))
             .route(catalog -> catalog.path("/api/v1/catalog*", "/api/v1/catalog/*")
+                .filters(f -> f.circuitBreaker(c -> c.setName("catalogCB")
+                    .setFallbackUri("forward:/catalog-failover")
+                    .setRouteId("cat-failover")))
                 .uri("http://localhost:8086"))
+            .route(catalogFailover -> catalogFailover.path("/catalog-failover/**")
+                .uri("http://localhost:8087/catalog-failover"))
             .build();
     }
 }
